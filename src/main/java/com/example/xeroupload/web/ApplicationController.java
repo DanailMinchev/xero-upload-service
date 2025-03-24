a673ab565baa72aa2c4c3085b37cd17ac533dc8c
Danail <DanailMinchev@users.noreply.github.com>
2025-03-24 22:05:43 +0200

    feat(upload-form): implement simple upload form

--- src/main/java/com/example/xeroupload/web/ApplicationController.java
+++ src/main/java/com/example/xeroupload/web/ApplicationController.java
@@ -2,6 +2,10 @@ package com.example.xeroupload.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ApplicationController {
@@ -9,6 +13,17 @@ public class ApplicationController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute(
+                "message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!"
+        );
+
+        return "redirect:/";
     }
 
 }
